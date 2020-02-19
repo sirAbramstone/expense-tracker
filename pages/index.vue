77fdb1ce9935bc0cb610c1152ba5ef86e21c59f2
Danil Abramov <danabramov@aicrobotics.ru>
2020-02-19 17:06:39 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  section.container
+</template>
+
+<script lang="ts">
+export default {
+  components: {}
+}
+</script>
+
+<style>
+  .container {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .title {
+    font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+    display: block;
+    font-weight: 300;
+    font-size: 100px;
+    color: #35495e;
+    letter-spacing: 1px;
+  }
+</style>
