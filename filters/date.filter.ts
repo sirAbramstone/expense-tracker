fcc28daa343ff029bff9f0ee623bb39a816a03fb
Danil Abramov <danabramov@aicrobotics.ru>
2020-04-16 15:00:49 +0300

    feat: correct date filter v2

--- filters/date.filter.ts
+++ filters/date.filter.ts
@@ -1,13 +1,11 @@
 import { DateOpt } from '~/interfaces/DateOpt';
 
-export function dateFilter(value: any, format = 'date'): string {
+export function dateFilter(value: Date | string, format = 'date'): string {
   const options: DateOpt = {};
   let date = value;
 
   if (typeof date === 'string') {
-    const dateArr: number[] = date.split('-').map((d) => Number(d));
-    dateArr[1] = dateArr[1] - 1;
-    date = new Date(...dateArr);
+    date = new Date(date);
   }
 
   if (format.includes('date')) {
